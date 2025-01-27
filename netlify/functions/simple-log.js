a68732898787fb5df7b73ed7320589cd675bea41
OppaismPreacher <domben2003@gmail.com>
2025-01-27 23:47:15 +0100

    Update simple-log.js

--- netlify/functions/simple-log.js
+++ netlify/functions/simple-log.js
@@ -1,10 +1,10 @@
 // simple-log.js
 exports.handler = async (event, context) => {
-    console.log("Simple function was triggered!");
+    console.log("Simple function was triggered :):):):):):):)!");
   
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Function ran successfully :):):):):):):) !" }),
+      body: JSON.stringify({ message: "Function ran successfully!" }),
       headers: {
         "Access-Control-Allow-Origin": "*", // Allow any origin
         "Access-Control-Allow-Methods": "GET, POST", // Allow specific methods
